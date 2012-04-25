3fa74ef0ea50a840ad26b063c6b742d6bfb34877
Ollie <2753508+oclancy@users.noreply.github.com>
2012-04-25 13:38:26 +0100

    Initial

---
+++ HitchinExchange.UI/FieldType.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+
+namespace HitchinExchange.UI
+{
+    public class FieldValue
+    {
+        public string DisplayValue { get; set; }
+        public string ActualValue { get; set; }
+    }
+
+    public class FieldType
+    {
+        public int Number { get; set; }
+
+        public string Name { get; set; }
+
+        public IEnumerable<FieldValue> Values { get; set; }
+
+        public string FixType { get; set; }
+    }
+}
---
+++ NewOrderMessageHandler/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace NewOrderMessageHandler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cf = new ConnectionFactory();
+
+            var m_mqClientConn = cf.CreateConnection();
+
+            var m_mqModel = m_mqClientConn.CreateModel();
+
+            var procId = System.Diagnostics.Process.GetCurrentProcess().Id;
+
+            m_mqModel.QueueDeclare(procId.ToString(), true, false, true, null);
+
+            m_mqModel.QueueBind(procId.ToString(), "Hitchin", string.Format("{0}.{1}.{2}", "HITCHIN", "NewOrderMessage", "#"));
+
+            var subs = new RabbitMQ.Client.MessagePatterns.Subscription(m_mqModel, procId.ToString());
+
+            foreach (BasicDeliverEventArgs ev in subs)
+            {
+                Console.WriteLine(ev.ToString());
+            }
+        }
+    }
+}
