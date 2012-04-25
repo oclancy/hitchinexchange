fe2b9ff432bf64f330aba19adbd722a8cf20c90d
Ollie <2753508+oclancy@users.noreply.github.com>
2012-04-25 14:11:11 +0100

    deleete

--- HitchinExchange_Native/Stdafx.cpp
+++
@@ -1,5 +0,0 @@
-// stdafx.cpp : source file that includes just the standard includes
-// HitchinExchange_Native.pch will be the pre-compiled header
-// stdafx.obj will contain the pre-compiled type information
-
-#include "stdafx.h"
